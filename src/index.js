3a9ebf3f9b00f9e989bf9475c14863f584c4e56a
KAICHA-KL <kaicha.reality@gmail.com>
2025-03-11 18:43:30 +0530

    Initial setup with webpack and tailwind

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// Import CSS files
+
+import './output.css';
+
+// Any JavaScript you want to include can go here
+console.log('Webpack is working!');
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  devServer: {
+    static: './dist',
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/src.html',
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'output.css',
+    }),
+  ],
+};
